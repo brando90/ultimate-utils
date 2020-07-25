c448050cbe93331748e369ce1948b754794f6f29
Brian Ko <briankosw@gmail.com>
2020-07-25 16:00:33 +0900

    Fixed logging and parsing bug

--- ultimate-utils-project/uutils/__init__.py
+++ ultimate-utils-project/uutils/__init__.py
@@ -48,10 +48,10 @@ def parse_args():
         "--exp-dir", type=str, help="directory for the experiment results"
     )
     parser.add_argument(
-        "--num-classes", help="number of classes in dataset", metavar="N"
+        "--num-classes", type=int, help="number of classes in dataset", metavar="N"
     )
     parser.add_argument(
-        "--seed", help="seed for deterministic experimenting", default=61820
+        "--seed", type=int, help="seed for deterministic experimenting", default=61820
     )
     parser.add_argument(
         "--epochs", type=int, help="number of epochs of training", metavar="E"
@@ -117,7 +117,7 @@ def get_logger(log_path, log_filename):
     console_handler.setLevel(logging.DEBUG)
     # Create formatter
     formatter = logging.Formatter(
-        "%(asctime)s - %(level)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+        "%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
     )
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
