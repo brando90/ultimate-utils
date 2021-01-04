4bbdcc37cb3fd721592f3a83fd71ad72e1a84cdb
Brando Miranda Garay <brandojazz@gmail.com>
2021-01-04 16:47:48 -0600

    reorganized the ml4coq library

--- python_pg.py
+++ python_pg.py
@@ -7737,3 +7737,62 @@ if result := do_something():
         do_more(result)
 
 [y := f(x), y**2, y**3]
+
+# %%
+
+from lark import Lark
+
+#
+grammar = '''start: WORD "," WORD "!"
+
+            %import common.WORD   // imports from terminal library
+            %ignore " "           // Disregard spaces in text
+         '''
+# grates parser
+l = Lark(grammar)
+
+print(l.parse("Hello, World!"))
+
+# %%
+
+from lark import Lark
+import lark
+
+grammar = """
+start: term
+
+term: apply
+    | const
+    | free
+    | var
+    | bound
+    | abs
+apply: "(apply " term " " term ")"
+const: "(const " MYSTR  ")"
+free: "(free " MYSTR ")"
+var: "(var " MYSTR ")"
+bound: "(bound " MYSTR ")"
+abs: "(abs " MYSTR " " term ")"
+MYSTR: LETTER (LETTER | "." | "_" | DIGIT)*
+
+%import common.WORD
+%import common.DIGIT
+%import common.LETTER
+%ignore " "
+"""
+parser = Lark(grammar)
+tree1 = parser.parse("(apply (const HOL.Trueprop) (apply (apply (const HOL.implies) (apply (apply (const HOL.conj) (free A)) (free B))) (apply (apply (const HOL.conj) (free B)) (free A))))")
+print(parser.parse("(apply (const HOL.Trueprop) (apply (apply (const HOL.implies) (apply (apply (const HOL.conj) (free A)) (free B))) (apply (apply (const HOL.conj) (free B)) (free A))))"))
+print(tree1.pretty())
+
+class IncreaseAllNumbers(lark.Transformer):
+    def _call_userfunc(self, tree, children):
+        # to do I will need to do something to get the type of variables
+        # because the variables' types are not attached yet
+        return
+
+    def _call_userfunc_token(self, c):
+        print(c)
+
+IncreaseAllNumbers(visit_tokens=True).transform(tree1)
+
