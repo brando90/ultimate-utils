2c5f737b43329bd53b92cc7153319334db67bb34
Brando Miranda Garay <brandojazz@gmail.com>
2021-05-04 11:53:11 -0500

    added the FILEID for syntactic model

--- run_tb.sh
+++ run_tb.sh
@@ -7,7 +7,7 @@
 # local_dir=$(python -c "import sys; print('ho' + str(sys.argv[1]))" $1)
 
 # -- transform the remote tb path to local path
-local_dir=$(python /Users/brando/ultimate-utils/ultimate-utils-project/execute_tensorboard.py $1)
+local_dir=$(python /Users/brando/ultimate-utils/ultimate-utils-proj-src/execute_tensorboard.py $1)
 # local_dir=$(python -c "from pathlib import Path; import sys; print(Path(sys.argv[1].replace('/home/miranda9/', '~/')).expanduser())" $1)
 
 # -- run the new remote tb locally
