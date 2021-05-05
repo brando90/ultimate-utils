84817939928db6b21cfea24cf1af7ef5a3a6dc87
Brando Miranda Garay <brandojazz@gmail.com>
2021-05-05 17:26:51 -0500

    put hack to get out of batches that result in errors so that semantic training works

--- python_pg.py
+++ python_pg.py
@@ -10370,3 +10370,23 @@ print(lst[:1])
 print(lst[:2])
 
 # its non inclusive
+
+#%%
+
+import dgl.data
+
+dataset = dgl.data.CoraGraphDataset()
+print('Number of categories:', dataset.num_classes)
+
+#%%
+
+import dgl
+import numpy as np
+import torch
+
+g = dgl.graph(([0, 0, 0, 0, 0], [1, 2, 3, 4, 5]), num_nodes=6)
+# Equivalently, PyTorch LongTensors also work.
+g = dgl.graph((torch.LongTensor([0, 0, 0, 0, 0]), torch.LongTensor([1, 2, 3, 4, 5])), num_nodes=6)
+
+# You can omit the number of nodes argument if you can tell the number of nodes from the edge list alone.
+g = dgl.graph(([0, 0, 0, 0, 0], [1, 2, 3, 4, 5]))
