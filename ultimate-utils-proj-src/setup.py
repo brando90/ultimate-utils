b38cb42f1008196207ef0842698a0a8c508a21bf
Brando Miranda Garay <brandojazz@gmail.com>
2021-08-31 16:33:10 -0500

    updating automatic install for mac (and hopefully uiuc clusters too)

--- ultimate-utils-proj-src/setup.py
+++ ultimate-utils-proj-src/setup.py
@@ -9,7 +9,7 @@ setup(
     author='Brando Miranda',
     author_email='brandojazz@gmail.com',
     license='MIT',
-    packages=find_packages(), # imports all modules (folder with __init__.py) & python files in this folder (since defualt args are . and empty exculde i.e. () )
+    packages=find_packages(),  # imports all modules (folder with __init__.py) & python files in this folder (since defualt args are . and empty exculde i.e. () )
     install_requires=['dill',
                       'networkx>=2.5',
                       'scipy',
@@ -19,6 +19,7 @@ setup(
                       'tensorboard',
                       'pandas',
                       'progressbar2',
+                      'transformers',
 
                       'pygraphviz']
 )
