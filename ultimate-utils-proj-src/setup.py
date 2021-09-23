bd47ded55a26c783a236671e21116762f69de78e
Brando Miranda Garay <brandojazz@gmail.com>
2021-09-23 12:31:02 -0500

    actually I am forcing exactly 0.10.1 since that is what I am doing for the rest of the pytorch depdencies, perhaps I will change them all to >= the version there or update it as pytorch releases new versions or how they work in the hpcs gpus

--- ultimate-utils-proj-src/setup.py
+++ ultimate-utils-proj-src/setup.py
@@ -18,7 +18,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='ultimate-utils',  # project name
-    version='0.4.0',
+    version='0.5.0',
     description='Brandos ultimate utils for science, machine learning and AI',
     long_description=README,
     long_description_content_type="text/markdown",
@@ -38,7 +38,7 @@ setup(
                       'scipy',
                       'scikit-learn',
                       'lark-parser',
-                      'torchtext>=0.10.1',
+                      'torchtext==0.10.1',
                       'tensorboard',
                       'pandas',
                       'progressbar2',
