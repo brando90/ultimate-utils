b39e3d97cf474902016d7a130e137dc263cec287
Brando Miranda Garay <brandojazz@gmail.com>
2021-11-08 17:00:16 -0600

    working on my custom matplotlib (not seaborn, sns) code for plots with error bands

--- ultimate-utils-proj-src/uutils/plot/__init__.py
+++ ultimate-utils-proj-src/uutils/plot/__init__.py
@@ -12,6 +12,7 @@ Array = Union[list, np.ndarray]
 MY_DEFAULT_BLUE: str = '#1f77b4'  # I like this blue but it might change whats default,
 MDB: str = MY_DEFAULT_BLUE
 
+
 def plot_quick(y: Array, xlabel: str, ylabel: str, linewidth: float = 2.0, show: bool = False, save_plot: bool = False,
                plot_name: str = 'plot'):
     """
@@ -28,9 +29,10 @@ def plot_quick(y: Array, xlabel: str, ylabel: str, linewidth: float = 2.0, show:
     if show:
         plt.show()
 
+
 def _plot(x: Array, y: Array, xlabel: str, ylabel: str,
-         linewidth: float = 2.0, show: bool = False,
-         save_plot: bool = False, plot_filename: str = 'plot', title: Optional[str] = None):
+          linewidth: float = 2.0, show: bool = False,
+          save_plot: bool = False, plot_filename: str = 'plot', title: Optional[str] = None):
     """
     Plots y against x.
     """
@@ -46,6 +48,7 @@ def _plot(x: Array, y: Array, xlabel: str, ylabel: str,
         save_to_desktop(plot_filename)
     if show:
         plt.show()
+
 
 def plot(x: Array, y: Array, xlabel: str, ylabel: str,
          linewidth: float = 2.0, show: bool = False,
@@ -81,6 +84,23 @@ def plot(x: Array, y: Array, xlabel: str, ylabel: str,
     if show:
         plt.show()
 
+
+def plot_with_error_bands(x: np.ndarray, y: np.ndarray, yerr: np.ndarray,
+                          color: str = None, ecolor: str = None,
+                          curve_label: str = 'mean with error bars',
+                          error_band_label: str = 'error band',
+                          linewidth: float = 2.0
+                          ):
+    fig, axs = plt.subplots(nrows=1, ncols=1, sharex=True, tight_layout=True)
+    axs.grid()
+    plt.errorbar(x=x, y=y, yerr=yerr, color='tab:blue', ecolor='tab:blue',
+                 capsize=3, linewidth=linewidth, label='mean with error bars')
+    plt.fill_between(x=x, y1=y - yerr, y2=y + yerr, alpha=0.2, label='error band')
+    plt.legend()
+
+    # plt.show()
+
+
 def save_to_desktop(plot_name: str = 'plot'):
     """
     Assuming you have not called show, saves it to local users desktop as a png, svg & pdf.
@@ -89,6 +109,7 @@ def save_to_desktop(plot_name: str = 'plot'):
     plt.savefig(root / f'{plot_name}.png')
     plt.savefig(root / f'{plot_name}.svg')
     plt.savefig(root / f'{plot_name}.pdf')
+
 
 def save_to_home(plot_name: str = 'plot'):
     """
@@ -99,6 +120,7 @@ def save_to_home(plot_name: str = 'plot'):
     plt.savefig(root / f'{plot_name}.svg')
     plt.savefig(root / f'{plot_name}.pdf')
 
+
 def save_to(root: Path, plot_name: str = 'plot'):
     """
     Assuming there is a plot in display, saves it to local users desktop users desktop as a png, svg & pdf.
@@ -108,9 +130,11 @@ def save_to(root: Path, plot_name: str = 'plot'):
     plt.savefig(root / f'{plot_name}.svg')
     plt.savefig(root / f'{plot_name}.pdf')
 
+
 # - seaborn
 
 LayerIdentifier = str
+
 
 def _list_order_dict2data_frame(data: list[OrderedDict[LayerIdentifier, float]], metric: str):
     """
@@ -119,6 +143,8 @@ def _list_order_dict2data_frame(data: list[OrderedDict[LayerIdentifier, float]],
     so it makes sure it has the metric repeated for all sample values basically in one single table.
     """
     from pandas import DataFrame
+    # - for each data value in [B, L] create a table augmented with the name for that data (which associates all the
+    # values for one curve together in the table)
     B: int = len(list)  # e.g. data.shape[0] number of columns
     L: int = len(list)  # e.g. data.shape[1] number of rows
     for row in range(B):  # b
@@ -150,6 +176,7 @@ def _list_metrics_full_data2data_frame(dist_metric2data: OrderedDict[str, list[O
         data_df = data_df.join(new_data_df)
     return data_df
 
+
 def plot_seaborn_table_with_metric(dist_metric2data: OrderedDict[str, list[OrderedDict[LayerIdentifier, float]]]):
     """
     The main idea of this function is that we have a collection of values in [B, L] e.g. B sim/dist values for each
@@ -164,6 +191,12 @@ def plot_seaborn_table_with_metric(dist_metric2data: OrderedDict[str, list[Order
     Notes:
         - hue parameter: this specifies which rows in the df to bunch as a [Samples, x-axis-values] so to consider
         that as the data matrix for a specific curve.
+
+    x, yvectors or keys in data
+    Variables that specify positions on the x and y axes.
+
+    huevector or key in data
+    Grouping variable that will produce lines with different colors. Can be either categorical or numeric, although color mapping will behave differently in latter case
     """
     import seaborn as sns
     from pandas import DataFrame
@@ -174,21 +207,80 @@ def plot_seaborn_table_with_metric(dist_metric2data: OrderedDict[str, list[Order
     sns.lineplot(x='layer_name', y='sample_val', hue='metric', data=data_df, err_style='band')
     sns.lineplot(x='layer_name', y='sample_val', hue='metric', data=data_df, err_style='band')
 
-# - tests
 
-def save_plot_test():
+def plot_seaborn_curve_with_x_values_y_values(x: np.ndarray, y: np.ndarray,
+                                              xlabel: str, ylabel: str,
+                                              title: str,
+                                              curve_label: Optional[str] = None,
+                                              err_style: str = 'band',
+                                              marker: Optional[str] = 'x',
+                                              dashes: bool = False,
+                                              show: bool = False
+                                              ):
     """
-    Should show the plot and also save three none-empty plot.
+    Given a list of x values in a range with num_x_values number of x values in that range and the corresponding samples
+    for each specific x value (so [samples_per_x] for each value of x giving in total a matrix of size
+    [samples_per_x, num_x_values]), plot aggregates of them with error bands.
+    Note that the main assumption is that each x value has a number of y values corresponding to it (likely due to noise
+    for example).
+
+
+    Note:
+        - if you want string in the specific x axis point do
+        sns.lineplot(x=np.tile([f'Layer{i}' for i in range(1, num_x+1)], rep_per_x),...) assuming the x values are the
+        layers. https://stackoverflow.com/questions/69888181/how-to-show-error-bands-for-pure-matrices-samples-x-range-with-seaborn-error/69889619?noredirect=1#comment123544763_69889619
+        - note you can all this function multiple times to insert different curves to your plot.
+        - note its recommended call show only for if you have one or at the final curve you want to add.
+        - if you want bands and bars it might work if you call this function twice but using the bar and band argument
+        for each call.
+
+    ref:
+        - https://stackoverflow.com/questions/69888181/how-to-show-error-bands-for-pure-matrices-samples-x-range-with-seaborn-error/69889619?noredirect=1#comment123544763_69889619
+
+    :param x: [num_x_values]
+    :param y: [samples_per_x, num_x_values]
+    :param xlabel:
+    :param ylabel:
+    :param title:
+    :param curve_label:
+    :param err_style:
+    :param marker:
+    :param dashes:
+    :param show:
+    :return:
     """
-    x = [1, 2, 3]
-    y = [2, 3, 4]
-    plot(x, y, 'x', 'y', show=True, save_plot=True)
+    import seaborn as sns
+    samples_per_x: int = y.shape[0]
+    num_x_values: int = x.shape[0]
+    assert (num_x_values == y.shape[1]), f'We are plotting aggreagates for one specific value of x multple values of y,' \
+                                         f'thus we need to have the same number of x values match in the x axis.'
+
+    # - since seaborn expects a an x value paired with it's y value, let's flatten the y's and make sure the corresponding
+    # x value is aligned with it's y value [num_x_values * samples_per_x]
+    x: np.ndarray = np.tile(x,
+                            samples_per_x)  # np.tile = Construct an array by repeating A the number of times given by reps.
+    assert (x.shape == (num_x_values * samples_per_x,))
+    y: np.ndarray = np.ravel(y)  # flatten the y's to match the x values to have the x to it's corresponding y
+    assert (y.shape == (num_x_values * samples_per_x,))
+    assert (x.shape == y.shape)
+
+    # - plot
+    ax = sns.lineplot(x=x, y=y, err_style=err_style, label=curve_label, marker=marker, dashes=dashes)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    if show:
+        plt.show()
 
 
-def my_example_seaborn_error_bands():
+# - examples
+
+def my_example_seaborn_error_bands_example():
     """
     An example of how to plot my [B, L] for each metric.
 
+    https://stackoverflow.com/questions/69888181/how-to-show-error-bands-for-pure-matrices-samples-x-range-with-seaborn-error
+    https://seaborn.pydata.org/generated/seaborn.lineplot.html
 
     https://seaborn.pydata.org/tutorial/relational.html#relational-tutorial
 
@@ -197,7 +289,11 @@ def my_example_seaborn_error_bands():
     https://github.com/knathanieltucker/seaborn-weird-parts/commit/3e571fd8e211ea04b6c9577fd548e7e532507acf
     https://github.com/knathanieltucker/seaborn-weird-parts/blob/3e571fd8e211ea04b6c9577fd548e7e532507acf/tsplot.ipynb
 
-    https://seaborn.pydata.org/examples/errorband_lineplots.html
+    x, yvectors or keys in data
+    Variables that specify positions on the x and y axes.
+
+    huevector or key in data
+    Grouping variable that will produce lines with different colors. Can be either categorical or numeric, although color mapping will behave differently in latter case
     """
     import numpy as np
     import seaborn as sns
@@ -251,6 +347,7 @@ def my_example_seaborn_error_bands():
 
     plt.show()
 
+
 def default_seabron_example():
     """
     https://seaborn.pydata.org/examples/errorband_lineplots.html
@@ -265,13 +362,175 @@ def default_seabron_example():
     print(fmri)
 
     # plot
-    sns.lineplot(x="timepoint", y="signal",  hue="region", style="event", data=fmri)
+    sns.lineplot(x="timepoint", y="signal", hue="region", style="event", data=fmri)
     plt.show()
 
 
-if __name__ == '__main__':
-    save_plot_test()
-    # my_example_seaborn_error_bands()
-    # default_seabron_example()
-    print('Done, success! \a')
+def custom_errorbands_without_seaborn_example():
+    """
+    pure matplot lib error bands.
 
+    https://stackoverflow.com/questions/55368485/draw-error-shading-bands-on-line-plot-python?noredirect=1&lq=1
+    """
+
+    import numpy as np  # v 1.19.2
+    import matplotlib.pyplot as plt  # v 3.3.2
+
+    rng = np.random.default_rng(seed=1)
+
+    x = np.linspace(0, 5 * np.pi, 50)
+    y = np.sin(x)
+    # error = np.random.normal(0.1, 0.02, size=x.shape) # I leave this out
+    nb_yfuncs = 25
+    ynoise = rng.normal(1, 0.1, size=(nb_yfuncs, y.size))
+    yfuncs = nb_yfuncs * [y] + ynoise
+
+    # fig, ax = plt.subplots(figsize=(10,4))
+    # for yfunc in yfuncs:
+    #     plt.plot(x, yfunc, 'k-')
+    #
+    # plt.show()
+
+    ymean = yfuncs.mean(axis=0)
+    ymin = yfuncs.min(axis=0)
+    ymax = yfuncs.max(axis=0)
+    yerror = np.stack((ymean - ymin, ymax - ymean))
+
+    fig, ax = plt.subplots(figsize=(10, 4))
+    plt.fill_between(x, ymin, ymax, alpha=0.2, label='error band')
+    plt.errorbar(x, ymean, yerror, color='tab:blue', ecolor='tab:blue',
+                 capsize=3, linewidth=1, label='mean with error bars')
+    plt.legend()
+
+    plt.show()
+
+
+def seaborn_multiple_curves_with_only_matrices_example():
+    """
+    Plot multple curves with error bands only having data matrices of the samples [num_samples, x_values_num_in_range] and
+    x values in a given range with x_values_num_in_range as the number of x-values in that range.
+
+    Ref:
+        - https://stackoverflow.com/questions/69888181/how-to-show-error-bands-for-pure-matrices-samples-x-range-with-seaborn-error/69889619#69889619
+    """
+    import numpy as np
+    import seaborn as sns
+    from matplotlib import pyplot as plt
+
+    print(sns)
+
+    np.random.seed(22)
+    sns.set(color_codes=True)
+
+    # the number of x values to consider in a given range e.g. [0,1] will sample 10 raw features x sampled at in [0,1] interval
+    num_x: int = 10
+    # the repetitions for each x feature value e.g. multiple measurements for sample x=0.0 up to x=1.0 at the end
+    rep_per_x: int = 5
+    total_size_data_set: int = num_x * rep_per_x
+    print(f'{total_size_data_set=}')
+    # - create fake data set
+    # only consider 10 features from 0 to 1
+    x = np.linspace(start=0.0, stop=1.0, num=num_x)
+
+    # to introduce fake variation add uniform noise to each feature and pretend each one is a new observation for that feature
+    noise_uniform: np.ndarray = np.random.rand(rep_per_x, num_x)
+    # same as above but have the noise be the same for each x (thats what the 1 means)
+    noise_normal: np.ndarray = np.random.randn(rep_per_x, 1)
+    # signal function
+    sin_signal: np.ndarray = np.sin(x)
+    cos_signal: np.ndarray = np.cos(x)
+    # [rep_per_x, num_x]
+    y1: np.ndarray = sin_signal + noise_uniform + noise_normal
+    y2: np.ndarray = cos_signal + noise_uniform + noise_normal
+
+    # - since seaborn expects a an x value paired with it's y value, let's flatten the y's and make sure the corresponding
+    # x value is alined with it's y value.
+    x: np.ndarray = np.tile(x,
+                            rep_per_x)  # np.tile = Construct an array by repeating A the number of times given by reps.
+    y1: np.ndarray = np.ravel(y1)  # flatten the y's to match the x values to have the x to it's corresponding y
+    y2: np.ndarray = np.ravel(y2)  # flatten the y's to match the x values to have the x to it's corresponding y
+
+    # - plot
+    err_style = 'band'
+    # err_style = 'bars'
+    ax = sns.lineplot(x=x, y=y1, err_style=err_style, label='sin', marker='x', dashes=False)
+    ax = sns.lineplot(x=x, y=y2, err_style=err_style, label='cos', marker='x', dashes=False)
+    plt.title('Sin vs Cos')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    plt.show()
+
+
+# - tests
+
+def save_plot_test():
+    """
+    Should show the plot and also save three none-empty plot.
+    """
+    x = [1, 2, 3]
+    y = [2, 3, 4]
+    plot(x, y, 'x', 'y', show=True, save_plot=True)
+
+
+def plot_seaborn_curve_with_x_values_y_values_test():
+    # the number of x values to consider in a given range e.g. [0,1] will sample 10 raw features x sampled at in [0,1] interval
+    num_x: int = 10
+    # the repetitions for each x feature value e.g. multiple measurements for sample x=0.0 up to x=1.0 at the end
+    rep_per_x: int = 5
+    total_size_data_set: int = num_x * rep_per_x
+    print(f'{total_size_data_set=}')
+    # - create fake data set
+    # only consider 10 features from 0 to 1
+    x = np.linspace(start=0.0, stop=1.0, num=num_x)
+
+    # to introduce fake variation add uniform noise to each feature and pretend each one is a new observation for that feature
+    noise_uniform: np.ndarray = np.random.rand(rep_per_x, num_x)
+    # same as above but have the noise be the same for each x (thats what the 1 means)
+    noise_normal: np.ndarray = np.random.randn(rep_per_x, 1)
+    # signal function
+    sin_signal: np.ndarray = np.sin(x)
+    cos_signal: np.ndarray = np.cos(x)
+    # [rep_per_x, num_x]
+    y1: np.ndarray = sin_signal + noise_uniform + noise_normal
+    y2: np.ndarray = cos_signal + noise_uniform + noise_normal
+
+    plot_seaborn_curve_with_x_values_y_values(x=x, y=y1, xlabel='x', ylabel='y', title='Sin vs Cos')
+    plot_seaborn_curve_with_x_values_y_values(x=x, y=y2, xlabel='x', ylabel='y', title='Sin vs Cos')
+    plt.show()
+
+def plot_with_error_bands_test():
+    import numpy as np  # v 1.19.2
+    import matplotlib.pyplot as plt  # v 3.3.2
+
+    # the number of x values to consider in a given range e.g. [0,1] will sample 10 raw features x sampled at in [0,1] interval
+    num_x: int = 10
+    # the repetitions for each x feature value e.g. multiple measurements for sample x=0.0 up to x=1.0 at the end
+    rep_per_x: int = 5
+    total_size_data_set: int = num_x * rep_per_x
+    print(f'{total_size_data_set=}')
+    # - create fake data set
+    # only consider 10 features from 0 to 1
+    x = np.linspace(start=0.0, stop=1.0, num=num_x)
+
+    # to introduce fake variation add uniform noise to each feature and pretend each one is a new observation for that feature
+    noise_uniform: np.ndarray = np.random.rand(rep_per_x, num_x)
+    # same as above but have the noise be the same for each x (thats what the 1 means)
+    noise_normal: np.ndarray = np.random.randn(rep_per_x, 1)
+    # signal function
+    sin_signal: np.ndarray = np.sin(x)
+    cos_signal: np.ndarray = np.cos(x)
+    # [rep_per_x, num_x]
+    y1: np.ndarray = sin_signal + noise_uniform + noise_normal
+    y2: np.ndarray = cos_signal + noise_uniform + noise_normal
+
+    y1mean = y1.mean(axis=0)
+    y1error = y1.std(axis=0)
+    y2mean = y2.mean(axis=0)
+    y2error = y2.std(axis=0)
+
+if __name__ == '__main__':
+    # save_plot_test()
+    # default_seabron_example()
+    plot_seaborn_curve_with_x_values_y_values_test()
+    print('Done, success! \a')
