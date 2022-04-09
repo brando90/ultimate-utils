bcfc49e01ba9bde89e6ec72660da7e3e753d357b
Brando Miranda <brandojazz@gmail.com>
2022-04-08 20:58:35 -0500

    my hacky dataloader is ready, it wraps the data in l2l but gets it in the same way as torchmeta or a normal pytorch data loader. Does not support ddp or num_workers, its just a hack!

--- ultimate-utils-proj-src/uutils/torch_uu/dataloaders/meta_learning/helpers.py
+++ ultimate-utils-proj-src/uutils/torch_uu/dataloaders/meta_learning/helpers.py
@@ -33,6 +33,11 @@ def get_meta_learning_dataloader(args: Namespace) -> dict:
             from uutils.torch_uu.dataloaders.meta_learning.rfs_meta_learning_data_loaders import \
                 get_rfs_meta_learning_mi_dataloader
             args.dataloaders = get_rfs_meta_learning_mi_dataloader(args)
+        elif 'l2l_data' in str(args.data_path):
+            # note: this line is mainly intended for data ananlysis! not meant for ddp, see this if you want that but idk if it will work: https://github.com/learnables/learn2learn/issues/263
+            from uutils.torch_uu.dataloaders.meta_learning.l2l_to_torchmeta_dataloader import \
+                get_l2l_torchmeta_dataloaders
+            args.dataloaders: dict = get_l2l_torchmeta_dataloaders(args)
         else:
             raise ValueError(f'Not such task: {args.data_path}')
         return args.dataloaders
--- ultimate-utils-proj-src/uutils/torch_uu/dataloaders/meta_learning/l2l_to_torchmeta_dataloader.py
+++ ultimate-utils-proj-src/uutils/torch_uu/dataloaders/meta_learning/l2l_to_torchmeta_dataloader.py
@@ -19,13 +19,14 @@ from uutils.torch_uu.dataloaders.meta_learning.l2l_ml_tasksets import get_l2l_ta
 from learn2learn.vision.benchmarks import BenchmarkTasksets
 
 
-class TaskData
-
 def get_standard_pytorch_dataset_from_l2l_taskdatasets(tasksets: BenchmarkTasksets, split: str) -> Dataset:
     """
     Trying to do:
         type(args.tasksets.train.dataset.dataset)
         <class 'learn2learn.vision.datasets.cifarfs.CIFARFS'>
+
+    Example call:
+        dataset: Dataset = get_standard_pytorch_dataset_from_l2l_taskdatasets(args.tasksets)
 
     :param tasksets:
     :param split:
@@ -44,97 +45,129 @@ def get_standard_pytorch_dataset_from_l2l_taskdatasets(tasksets: BenchmarkTaskse
 
 def get_l2l_torchmeta_dataloaders(args: Namespace) -> dict:
     args.tasksets: BenchmarkTasksets = get_l2l_tasksets(args)
-    # dataloader = DataLoader(transformed_dataset, batch_size=4,
-    #                         shuffle=True, num_workers=0)
 
-    # does cleanly: args.tasksets.train.dataset.dataset
-    dataset: Dataset = get_standard_pytorch_dataset_from_l2l_taskdatasets(args.tasksets)
-    meta_valloader = DataLoader(dataset,
-                                batch_size=args.test_batch_size, shuffle=False, drop_last=False,
-                                num_workers=args.num_workers)
+    meta_trainloader = TorchMetaDLforL2L(args, split='train')
+    meta_valloader = TorchMetaDLforL2L(args, split='val')
+    meta_testloader = TorchMetaDLforL2L(args, split='test')
 
-    dataset = get_standard_pytorch_dataset_from_l2l_taskdatasets(args.tasksets)
-    meta_valloader = DataLoader(MetaImageNet(args=args, partition='val',
-                                             train_transform=train_trans,
-                                             test_transform=test_trans,
-                                             fix_seed=False),
-                                batch_size=args.test_batch_size, shuffle=False, drop_last=False,
-                                num_workers=args.num_workers)
-    dataset = get_standard_pytorch_dataset_from_l2l_taskdatasets(args.tasksets)
-    meta_valloader = DataLoader(MetaImageNet(args=args, partition='val',
-                                             train_transform=train_trans,
-                                             test_transform=test_trans,
-                                             fix_seed=False),
-                                batch_size=args.test_batch_size, shuffle=False, drop_last=False,
-                                num_workers=args.num_workers)
-    pass
+    args.dataloaders: dict = {'train': meta_trainloader, 'val': meta_valloader, 'test': meta_testloader}
+    return args.dataloaders
 
 
-def get_batch_from_l2l_into_torchmeta_format(args: Namespace):
+class TorchMetaDLforL2L:
     """
-    :return: something like [B, k*n, D] or [B, k*n, D] depending on your task.
-
-    note:
-        - task_dataset: TaskDataset = getattr(args.tasksets, split)
-        - recall a task is a "mini (spt) classification data set" e.g. with n classes and k shots (and it's qry set too)
-        - torchmeta example: https://tristandeleu.github.io/pytorch-meta/
+    Not intended or tested to work with ddp. For that extension see this: https://github.com/learnables/learn2learn/issues/263
     """
-    import learn2learn
-    from learn2learn.data import TaskDataset
 
-    shots = args.k_shots
-    ways = args.n_classes
-    # device = args.device
-    # meta_batch_size: int = max(self.args.batch_size // self.args.world_size, 1)
-    meta_batch_size: int = args.batch_size
+    def __init__(self, args, split: str):
+        self.args = args
+        assert split in ['train', 'val', 'test']
+        self.split = split
+        self.batch_size = None
 
-    task_dataset: TaskDataset = getattr(args.tasksets, split)
+    def __iter__(self):
+        # initialization code for iterator usually goes here, I don't think we need any
+        if self.split == 'train':
+            self.batch_size = self.args.batch_size
+        else:
+            self.split = 'validation' if self.split == 'val' else self.split
+            self.batch_size = self.args.batch_size_eval
+        return self
 
-    spt_x, spt_y, qry_x, qry_y = [], [], [], []
-    for task in range(meta_batch_size):
-        # - Sample all data data for spt & qry sets for current task: thus size [n*(k+k_eval), C, H, W] (or [n(k+k_eval), D])
-        task_data: list = task_dataset.sample()  # data, labels
-        data, labels = task_data
-        # data, labels = data.to(device), labels.to(device)  # note here do it process_meta_batch, only do when using original l2l training method
+    def __next__(self):
+        """
+        Returns a batch of tasks, the way that torchmeta would.
 
-        # Separate data into adaptation/evalutation sets
-        # [n*(k+k_eval), C, H, W] -> [n*k, C, H, W] and [n*k_eval, C, H, W]
-        (support_data, support_labels), (query_data, query_labels) = learn2learn.data.partition_task(
-            data=data,
-            labels=labels,
-            shots=shots,  # shots to separate to two data sets of size shots and k_eval
-        )
-        # checks coordinate 0 of size() [n*(k + k_eval), C, H, W]
-        assert support_data.size(0) == shots * ways, f' Expected {shots * ways} but got {support_data.size(0)}'
-        # checks [n*k] since these are the labels
-        assert support_labels.size() == torch.Size([shots * ways])
+        :return: {'train': (spt_x, spt_y), 'test': (qry_x, qry_y)} with spt_x of size [B, k*n, D]
+            and spt y_of size [B, k*n, D]. qry_x of size [B, k*n, D] and spt_y of size
+        depending on your task.
 
-        # append task to lists of tasks to ultimately create: [B, n*k, D], [B, n*k], [B, n*k_eval, D], [B, n*k_eval],
-        spt_x.append(support_data)
-        spt_y.append(support_labels)
-        qry_x.append(query_data)
-        qry_y.append(query_labels)
-    #
-    spt_x, spt_y, qry_x, qry_y = tensorify(spt_x), tensorify(spt_y), tensorify(qry_x), tensorify(qry_y)
-    assert spt_x.size(0) == torch.Size([meta_batch_size])
-    assert qry_x.size(0) == torch.Size([meta_batch_size])
-    # should be sizes [B, n*k, C, H, W] or [B,]
-    # - Return meta-batch of tasks
-    batch = {'train': (spt_x, spt_y), 'test': (qry_x, qry_y)}
-    return batch
+        note:
+            - task_dataset: TaskDataset = getattr(args.tasksets, split)
+            - recall a task is a "mini (spt) classification data set" e.g. with n classes and k shots (and it's qry set too)
+            - torchmeta example: https://tristandeleu.github.io/pytorch-meta/
+        """
+        import learn2learn
+        from learn2learn.data import TaskDataset
+
+        shots = self.args.k_shots
+        ways = self.args.n_classes
+        # meta_batch_size: int = max(self.args.batch_size // self.args.world_size, 1)
+        meta_batch_size: int = self.batch_size
+
+        task_dataset: TaskDataset = getattr(self.args.tasksets, self.split)
+
+        spt_x, spt_y, qry_x, qry_y = [], [], [], []
+        for task in range(meta_batch_size):
+            # - Sample all data data for spt & qry sets for current task: thus size [n*(k+k_eval), C, H, W] (or [n(k+k_eval), D])
+            task_data: list = task_dataset.sample()  # data, labels
+            data, labels = task_data
+            # data, labels = data.to(device), labels.to(device)  # note here do it process_meta_batch, only do when using original l2l training method
+
+            # Separate data into adaptation/evalutation sets
+            # [n*(k+k_eval), C, H, W] -> [n*k, C, H, W] and [n*k_eval, C, H, W]
+            (support_data, support_labels), (query_data, query_labels) = learn2learn.data.partition_task(
+                data=data,
+                labels=labels,
+                shots=shots,  # shots to separate to two data sets of size shots and k_eval
+            )
+            # checks coordinate 0 of size() [n*(k + k_eval), C, H, W]
+            assert support_data.size(0) == shots * ways, f' Expected {shots * ways} but got {support_data.size(0)}'
+            # checks [n*k] since these are the labels
+            assert support_labels.size() == torch.Size([shots * ways])
+
+            # append task to lists of tasks to ultimately create: [B, n*k, D], [B, n*k], [B, n*k_eval, D], [B, n*k_eval],
+            spt_x.append(support_data)
+            spt_y.append(support_labels)
+            qry_x.append(query_data)
+            qry_y.append(query_labels)
+        #
+        spt_x, spt_y, qry_x, qry_y = tensorify(spt_x), tensorify(spt_y), tensorify(qry_x), tensorify(qry_y)
+        assert spt_x.size(0) == meta_batch_size, f'Error, expected {spt_x.size(0)=} got {meta_batch_size=}.'
+        assert qry_x.size(0) == meta_batch_size, f'Error, expected {spt_x.size(0)=} got {meta_batch_size=}.'
+        # should be sizes [B, n*k, C, H, W] or [B,]
+        # - Return meta-batch of tasks
+        batch = {'train': (spt_x, spt_y), 'test': (qry_x, qry_y)}
+        return batch
 
 
 # - tests
 
+# def args_5cnn_cifarfs(args: Namespace) -> Namespace:
+#     """
+#     """
+#     from uutils.torch_uu.models.resnet_rfs import get_recommended_batch_size_cifarfs_resnet12rfs_body, \
+#         get_feature_extractor_conv_layers
+#     # - model
+#     args.model_option = '4CNN_l2l_cifarfs'
+#
+#     # - data
+#     args.data_option = 'cifarfs_rfs'  # no name assumes l2l, make sure you're calling get_l2l_tasksets
+#     args.data_path = Path('~/data/l2l_data/').expanduser()
+#     args.data_augmentation = 'rfs2020'
+#     args.augment_train = True
+#
+#     args.batch_size = 5
+#     args.batch_size_eval = args.batch_size
+#
+#     # - fix for backwards compatibility
+#     args = fix_for_backwards_compatibility(args)
+#     # - setup paths to ckpts for data analysis
+#     args = setup_args_path_for_ckpt_data_analysis(args, 'ckpt.pt')
+#     # - fill in the missing things and make sure things make sense for run
+#     args = uutils.setup_args_for_experiment(args)
+#     return args
+
 def l2l_example(meta_batch_size: int = 4, num_iterations: int = 5):
     from uutils.torch_uu import process_meta_batch
-    args: Namespace = Namespace(k_shots=5, n_classes=5, k_eval=15, data_option='cifarfs',
-                                data_path=Path('~/data/l2l_data/'))
+    args: Namespace = Namespace(k_shots=5, n_classes=5, k_eval=15, data_option='cifarfs_rfs',
+                                data_path=Path('~/data/l2l_data/'), batch_size=4, batch_size_eval=6,
+                                data_augmentation='rfs2020', device='cpu')
 
     dataloaders: dict = get_l2l_torchmeta_dataloaders(args)
     for split, dataloader in dataloaders.items():
         print(f'{split=}')
-        assert dataloaders[split] is dataloader
+        assert dataloader.split == split
         for batch in dataloader:
             train_inputs, train_targets = batch["train"]
             spt_x, spt_y, qry_x, qry_y = process_meta_batch(args, batch)
