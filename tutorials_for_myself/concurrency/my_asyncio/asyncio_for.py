b29a6232e1634ea6548032340302304854a7c3f6
Brando Miranda <miranda9@ibm.com>
2022-06-22 17:24:27 -0400

    made a nice plan of what to do for async for. I think its just a normal for loop but at the end of each loop of the (async) iteration it does an await something like await iter.__anext__() or something. Likely the iter is calling some expansive io-bound op to get the next element in the iteration. e.g. calling coq or calling a database, tcp thing or something

--- tutorials_for_myself/concurrency/my_asyncio/asyncio_for.py
+++ tutorials_for_myself/concurrency/my_asyncio/asyncio_for.py
@@ -1,21 +1,28 @@
 """
-1. https://realpython.com/async-io-python/#the-asyncawait-syntax-and-native-coroutines
-2. https://stackoverflow.com/questions/56161595/how-to-use-async-for-in-python
-3. https://stackoverflow.com/questions/67092070/why-do-we-need-async-for-and-async-with
+File demonstrating looping through asynchronous iterators.
 
-todo - async with, async for.
+Definitions:
+async for = allows to loop through an asynchronous iterator.
+
+plan:
+- my guess is a good plan is to go through the generator & yield example in real python: https://realpython.com/async-io-python/#the-asyncawait-syntax-and-native-coroutines
+
+Ref:
+    - https://stackoverflow.com/questions/56161595/how-to-use-async-for-in-python
+    - https://realpython.com/async-io-python/#the-asyncawait-syntax-and-native-coroutines
+    - (idk, don't like this one too much... https://stackoverflow.com/questions/67092070/why-do-we-need-async-for-and-async-with
+
+todo: create a concrete async forn example, likely contrasting it with a blocking for and how to "implement"
+    an equivalent for with no async for but instead with an for with awaits.
 """
 
 import asyncio
-from asyncio.events import AbstractEventLoop
 
 import time
 
-from typing import Coroutine
 
 def test1():
-    """
-    """
+    """"""
     pass
 
 if __name__ == '__main__':
