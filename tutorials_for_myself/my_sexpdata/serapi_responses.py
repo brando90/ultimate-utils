37238bebc9ff062a919519b267abe453f7a55cf9
Brando Miranda <miranda9@ibm.com>
2022-06-29 15:59:41 -0400

    extracted the ppt systematically using the coq-serapi protocol using the ocaml specification

--- tutorials_for_myself/my_sexpdata/serapi_responses.py
+++ tutorials_for_myself/my_sexpdata/serapi_responses.py
@@ -44,16 +44,73 @@ with parenthesis that looks like an array it gives it to you the intuitive way p
 array and not as a binary tree (at least not to the user).
 """
 # %%
-from sexpdata import loads, dumps
 
-res: str = "( ObjList ( (CoqString\"none\\n============================\\nforall x : bool, negb (negb x) = x\") )  )"
-print(res)
-x = loads(res)
-print(x)
-print()
-print(x[1][0][1])
-print()
+# from sexpdata import loads, dumps
+#
+# res: str = "( ObjList ( (CoqString\"none\\n============================\\nforall x : bool, negb (negb x) = x\") )  )"
+# print(res)
+# x = loads(res)
+# print(x)
+# print()
+# print(x[1][0][1])
+# print()
+#
+# res: str = "(x y z)"
+# y = loads(res)
+# print(y)
 
-res: str = "(x y z)"
-y = loads(res)
-print(y)
+#%%
+"""
+type answer = 
+| Feedback of feedback
+
+type feedback = {
+...
+contents : feedback_content;
+}
+
+type feedback_content = 
+...
+| Message of {
+level : Feedback.level;
+loc : Loc.t option;
+pp : Pp.t;
+str : string;
+}
+"""
+from pprint import pprint
+
+def extract_proof_term(serapi_response: str) -> str:
+    from sexpdata import loads
+    res: str = serapi_response
+    print(res)
+    _res: list = loads(res)
+    feedback: list = _res[-1]
+    assert len(feedback) == 4
+    print(f"{len(feedback)=}")
+    pprint(feedback)
+    contents: list = feedback[-1]
+    pprint(contents)
+    print(f'{len(contents)=}')
+    assert len(contents) == 2
+    message: list = contents[-1]
+    assert len(message) == 5
+    string: list = message[-1]
+    assert string[0].value() == 'str'
+    if isinstance(string[-1], str):
+        ppt: str = string[-1]
+    else:
+        ppt: str = string[-1].value()
+    print(ppt)
+    return ppt
+
+res0: str = """(Feedback((doc_id 0)(span_id 6)(route 0)(contents(Message(level Notice)(loc())(pp(Pp_box(Pp_hovbox 0)(Pp_tag constr.evar(Pp_glue((Pp_string ?Goal))))))(str ?Goal)))))\n"""
+print(res0)
+ppt0: str = extract_proof_term(res0)
+print(ppt0)
+
+res1: str = """(Feedback((doc_id 0)(span_id 8)(route 0)(contents(Message(level Notice)(loc())(pp(Pp_box(Pp_hovbox 1)(Pp_glue((Pp_string"(")(Pp_box(Pp_hovbox 0)(Pp_glue((Pp_box(Pp_hovbox 2)(Pp_glue((Pp_tag constr.keyword(Pp_string fun))(Pp_print_break 1 0)(Pp_box(Pp_hovbox 1)(Pp_glue((Pp_string"x : ")(Pp_tag constr.variable(Pp_string bool))))))))(Pp_print_break 1 0)(Pp_string =>)(Pp_print_break 1 0)(Pp_box(Pp_vbox 0)(Pp_glue((Pp_box(Pp_hvbox 0)(Pp_glue((Pp_tag constr.keyword(Pp_string match))(Pp_print_break 1 2)(Pp_box(Pp_hovbox 0)(Pp_glue((Pp_box(Pp_hovbox 0)(Pp_glue((Pp_tag constr.variable(Pp_string x))(Pp_print_break 1 0)(Pp_tag constr.keyword(Pp_string as))(Pp_print_break 1 0)(Pp_string b))))(Pp_print_break 1 0)(Pp_box(Pp_hovbox 2)(Pp_glue((Pp_tag constr.keyword(Pp_string return))(Pp_print_break 1 0)(Pp_box(Pp_hovbox 1)(Pp_glue((Pp_string"(")(Pp_box(Pp_hovbox 0)(Pp_glue((Pp_box(Pp_hovbox 2)(Pp_glue((Pp_tag constr.variable(Pp_string negb))(Pp_print_break 1 0)(Pp_box(Pp_hovbox 1)(Pp_glue((Pp_string"(")(Pp_box(Pp_hovbox 2)(Pp_glue((Pp_tag constr.variable(Pp_string negb))(Pp_print_break 1 0)(Pp_tag constr.variable(Pp_string b)))))(Pp_string")")))))))(Pp_tag constr.notation(Pp_string" ="))(Pp_print_break 1 0)(Pp_tag constr.variable(Pp_string b)))))(Pp_string")"))))))))))(Pp_print_break 1 0)(Pp_tag constr.keyword(Pp_string with)))))(Pp_print_break 1 0)(Pp_box(Pp_hovbox 4)(Pp_glue((Pp_string"| ")(Pp_box(Pp_hovbox 0)(Pp_glue((Pp_box(Pp_hovbox 0)(Pp_tag constr.variable(Pp_string true)))(Pp_string" =>"))))(Pp_print_break 1 0)(Pp_box(Pp_hovbox 0)(Pp_tag constr.evar(Pp_glue((Pp_string ?Goal))))))))(Pp_print_break 1 0)(Pp_box(Pp_hovbox 4)(Pp_glue((Pp_string"| ")(Pp_box(Pp_hovbox 0)(Pp_glue((Pp_box(Pp_hovbox 0)(Pp_tag constr.variable(Pp_string false)))(Pp_string" =>"))))(Pp_print_break 1 0)(Pp_box(Pp_hovbox 0)(Pp_tag constr.evar(Pp_glue((Pp_string ?Goal0))))))))(Pp_print_break 1 0)(Pp_tag constr.keyword(Pp_string end))))))))(Pp_string")")))))(str"(fun x : bool =>\\n match x as b return (negb (negb b) = b) with\\n | true => ?Goal\\n | false => ?Goal0\\n end)")))))\n"""
+print(res1)
+ppt1: str = extract_proof_term(res1)
+print(ppt1)
+
