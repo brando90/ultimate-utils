e89885f3a6b04343fa55d8b6bd69602d0de660b9
Brando Miranda <miranda9@ibm.com>
2022-08-09 12:05:43 -0400

    added datasets to setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ultimate-utils',  # project name
-    version='0.5.4',
+    version='0.5.5',
     description="Brando's ultimate utils for science, machine learning and AI",
     long_description=long_description,
     long_description_content_type="text/markdown",
