b678600a5e6b6c0a3617af7e1dd7ea33d295e54a
Brando Miranda <miranda9@ibm.com>
2022-08-15 15:36:25 -0400

    small changes to pg hf

---
+++ tutorials_for_myself/my_hf_hugging_face_pg/hf_dummy_data_pf.py
@@ -0,0 +1,14 @@
+#%%
+
+import numpy as np
+from datasets import Dataset
+
+
+seq_len, dataset_size = 512, 512
+dummy_data = {
+    "input_ids": np.random.randint(100, 30000, (dataset_size, seq_len)),
+    "labels": np.random.randint(0, 1, (dataset_size)),
+}
+ds = Dataset.from_dict(dummy_data)
+ds.set_format("pt")
+
