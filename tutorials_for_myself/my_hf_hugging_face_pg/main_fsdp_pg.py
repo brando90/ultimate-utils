d55287c2b15f986c21d7617a59851c588aa8e4b6
Brando Miranda <miranda9@ibm.com>
2022-08-17 10:25:20 -0400

    fsdp outline links for uutils

---
+++ tutorials_for_myself/my_hf_hugging_face_pg/main_fsdp_pg.py
@@ -0,0 +1,9 @@
+"""
+
+- blog: https://pytorch.org/blog/introducing-pytorch-fully-sharded-data-parallel-api/
+- search in this doc for "fsdp": https://huggingface.co/docs/transformers/main_classes/trainer#pytorch-fully-sharded-data-parallel
+
+extra:
+- all docs for efficient training on multiple gpus (no fsdp btw: https://huggingface.co/docs/transformers/perf_train_gpu_many#efficient-training-on-multiple-gpus)
+"""
+#%%
