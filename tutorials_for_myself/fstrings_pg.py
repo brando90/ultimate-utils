c86a12b7a241c0757fc0248f73631ff718382ae6
Brando Miranda <miranda9@ibm.com>
2022-08-18 16:46:31 -0400

    fstring format

--- tutorials_for_myself/fstrings_pg.py
+++ tutorials_for_myself/fstrings_pg.py
@@ -1,5 +1,8 @@
 # %%
-
+"""
+https://docs.python.org/3/library/stdtypes.html#str.format
+https://stackoverflow.com/questions/54351740/how-can-i-use-f-string-with-a-variable-not-with-a-string-literal
+"""
 template_fstring: str = 'folder_{split}'
 print(template_fstring.format(split='train'))
 print(template_fstring.format(split='test'))
