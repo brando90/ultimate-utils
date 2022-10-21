054e767f3ae135d4d89d29fb8e41394f8320171f
Brando Miranda <brandojazz@gmail.com>
2022-10-21 13:45:24 -0700

    forward pass for vit without testing done

--- ultimate-utils-proj-src/uutils/torch_uu/models/hf_uu/vit_uu.py
+++ ultimate-utils-proj-src/uutils/torch_uu/models/hf_uu/vit_uu.py
@@ -19,7 +19,14 @@ sub-layer input and normalized. In addition, we apply dropout to the sums of the
 positional encodings in both the encoder and decoder stacks. For the base model, we use a rate of
 Pdrop = 0.1
 ```
-**so before the addition and norm, we do dropout of multi-headed self attention (MHSE) sublayer & FC/MLP sublayer.**
+**so before the addition and norm, we do dropout of multi-headed self attention (MHSE) sublayer & FC/MLP sublayer.** (for vasmani trans)
+
+For ViT:
+```
+Dropout, when used, is applied after
+every dense layer except for the the qkv-projections and directly after adding positional- to patch
+embeddings.
+```
 
 # pseudocode for original transformer:
 x = get_seq()
@@ -88,6 +95,7 @@ def does_feature_extractor_have_params():
     from transformers import ViTModel
     model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
     print(f'{type(model)=}')
+    print(f'{isinstance(model, nn.Module)=}')
     # type(model)=<class 'transformers.models.vit.modeling_vit.ViTModel'>
     model.eval()
     model.to(device)
@@ -103,19 +111,22 @@ def does_feature_extractor_have_params():
     # - get encoding/embedding of im
     from transformers import ViTFeatureExtractor
     feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+    print(f'{feature_extractor=}')
     print(type(feature_extractor))
+    print(f'{isinstance(feature_extractor, nn.Module)=}')  # False! :D
     # class 'transformers.models.vit.feature_extraction_vit.ViTFeatureExtractor'>
     encoding = feature_extractor(images=im, return_tensors="pt")
+    print(f'{encoding=}')
     encoding.keys()
 
-    encoding['pixel_values'].shape
+    print(f"{encoding['pixel_values'].shape=}")
 
     # - forward pass (get logits!)
     pixel_values = encoding['pixel_values'].to(device)
-
     outputs = model(pixel_values)
+    print(f'{outputs=}')
     logits = outputs.logits
-    logits.shape
+    print(f'{logits.shape}=')
 
     # - (extra, get label name), empty function in class, point to tutorial
     prediction = logits.argmax(-1)
@@ -124,14 +135,70 @@ def does_feature_extractor_have_params():
 
 def cifar_vit():
     """meant for something completely self contained...if I have time. """
-    pass
+    # - get data and data loader
+    import torchvision
+    from pathlib import Path
+    root = Path('~/data/').expanduser()
+    train = torchvision.datasets.CIFAR100(root=root, train=True, download=True,
+                                          transform=torchvision.transforms.ToTensor(),
+                                          target_transform=lambda data: torch.tensor(data, dtype=torch.int))
+    test = torchvision.datasets.CIFAR100(root=root, train=False, download=True,
+                                         transform=torchvision.transforms.ToTensor(),
+                                         target_transform=lambda data: torch.tensor(data, dtype=torch.int))
+    from torch.utils.data import DataLoader
+    train_loader = DataLoader(train, batch_size=4)
+
+    # - get vit model & patcher
+    # matching models according to: https://github.com/NielsRogge/Transformers-Tutorials/blob/master/VisionTransformer/Quick_demo_of_HuggingFace_version_of_Vision_Transformer_inference.ipynb
+    # from transformers import ViTForImageClassification
+    # model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+    # feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+    # custom data set tutorial: https://colab.research.google.com/drive/1Z1lbR_oTSaeodv9tTm11uEhOjhkUx1L4?usp=sharing#scrollTo=5ql2T5PDUI1D
+    from transformers import ViTModel
+    model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
+    feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
+    # this blog also uses the in21k, https://huggingface.co/blog/fine-tune-vit
+    # cls layer
+    num_labels, cls_p_dropout = 100, 0.1  # seems they use 0.1 p_dropout for cosine scheduler, but also use weight decay...
+    dropout = nn.Dropout(cls_p_dropout)
+    classifier = nn.Linear(model.config.hidden_size, num_labels)
+    criterion = nn.CrossEntropyLoss()
+    device = torch.device(f"cuda:{0}" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    # -
+    epochs = 1
+    for epoch in range(epochs):
+        for batch_idx, batch in enumerate(train_loader):
+            # - process batch
+            x, y = batch
+            assert isinstance(x, torch.Tensor)
+            assert isinstance(y, torch.Tensor)
+            print(f'{x.size()=}')
+
+            # - forward pass
+            # n case of a NumPy array/PyTorch tensor, each image should be of shape (C, H, W), where C is a number of channels, H and W are image height and width.
+            # assert x.size() == torch.Size([C, H, W)) # if passed to the weird hf feature extractor module
+            # assert x.size(1) == 3
+            # x = feature_extractor(x)
+            # print(f'{x.size()=}')
+            out = model(x)
+            print(f'{out.size()=}')
+            # todo: Q, so do we do dropout after cls linear layer? Dropout, when used, is applied after every dense layer except for the the qkv-projections and directly after adding positional- to patch embeddings.
+            cls_token_emebd = out.last_hidden_state[:, 0]
+            print(f'{cls_token_emebd.size()=}')
+            output = dropout(cls_token_emebd)
+            logits = classifier(output)
+            # loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            loss = criterion(logits, y)
+            print(f'{loss=}')
+            # then you can do backward()
+            st()
 
 
 def hdb1_vit():
     """
     testing hdb1 with vit.
 
-python ~/ultimate-utils/ultimate-utils-proj-src/uutils/torch_uu/models/hf_uu/vit_uu.py
     """
     # - for determinism
     # random.seed(0)
@@ -203,6 +270,9 @@ def mi_vit():
 
 
 # -- Run experiment
+"""
+python ~/ultimate-utils/ultimate-utils-proj-src/uutils/torch_uu/models/hf_uu/vit_uu.py
+"""
 
 if __name__ == "__main__":
     import time
@@ -211,6 +281,7 @@ if __name__ == "__main__":
     start = time.time()
     # - run experiment
     does_feature_extractor_have_params()
+    # cifar_vit()
     # hdb1_vit()
     # mi_vit()
     # - Done
