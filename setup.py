d6ab948551ec2834ee37cbc587bcd775e18815ea
Brando Miranda <brandojazz@gmail.com>
2023-01-26 16:15:20 -0800

    removed the careless/lazy mistake of not making deep copies but that copies the model so its too expensive though correct, solution was to create a new meta learning object (agent) and assign the new learning rates there to avoid re-writing the old meta-learning object. Careful with mutation. Ok the bug needed to be fix at the meta-learner class since it didn't have that int being set. The deepcopy of an int is unneccessary but it will work. The issue was that the object was re-setting the args value which resetted everything globally. By setting it as a field that was only for that obj and not resetting the global args, it removed the issue. This is the issue with globals!

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
         'plotly',
         'wandb',
         'matplotlib',
-        'statsmodels'
+        # 'statsmodels'
         # - later check why we are not installing it...
         # 'seaborn'
         # 'nltk'
