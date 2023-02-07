e990e5459a38fa5348a063f9fc3732ee6f1d302a
Brando Miranda <brandojazz@gmail.com>
2023-02-07 12:14:55 -0800

    tutorial test for divs

--- ultimate-utils-proj-src/uutils/torch_uu/metrics/diversity/diversity.py
+++ ultimate-utils-proj-src/uutils/torch_uu/metrics/diversity/diversity.py
@@ -443,7 +443,13 @@ def get_task2vec_diversity_coefficient_from_embeddings(embeddings: list[task2vec
         - return div, ci
 
     # - get your data
-    embeddings: list[task2vec.Embedding] = get_random_data_todo()
+    embeddings: list[task2vec.Embedding] = get_task_embeddings_from_few_shot_dataloader(args,
+                                                                                    args.dataloaders,
+                                                                                    args.probe_network,
+                                                                                    num_tasks_to_consider=args.batch_size,
+                                                                                    split=split,
+                                                                                    classifier_opts=args.classifier_opts,
+                                                                                    )
 
     # - compute distance matrix & task2vec based diversity, to demo` task2vec, this code computes pair-wise distance between task embeddings
     distance_matrix: np.ndarray = task_similarity.pdist(embeddings, distance='cosine')
@@ -457,6 +463,24 @@ def get_task2vec_diversity_coefficient_from_embeddings(embeddings: list[task2vec
     print(f'Diversity: {(div, ci)=}')
     standardized_div: float = get_standardized_diversity_coffecient_from_pair_wise_comparison_of_tasks(distance_matrix)
     print(f'Standardised Diversity: {standardized_div=}')
+
+    import torch
+    import random
+
+    class RandomDataset(torch.utils.data.Dataset):
+        def __init__(self, num_samples):
+            self.num_samples = num_samples
+
+        def __getitem__(self, index):
+            data = torch.randn(1, 1) # generate random data
+            label = random.randint(0, 1) # generate random label
+            return data, label
+
+        def __len__(self):
+            return self.num_samples
+
+    dataset = RandomDataset(100) # 100 samples
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
     """
     distance_matrix: np.ndarray = task_similarity.pdist(embeddings, distance='cosine')
     div, ci = task_similarity.stats_of_distance_matrix(distance_matrix)
@@ -551,13 +575,67 @@ def compute_size_aware_div_test_go():
     print()
 
 
+def test_tutorial():
+    import torch
+    import random
+
+    class RandomDataset(torch.utils.data.Dataset):
+        def __init__(self, num_samples):
+            self.num_samples = num_samples
+
+        def __getitem__(self, index):
+            data = torch.randn(1, 1)  # generate random data
+            label = random.randint(0, 1)  # generate random label
+            return data, label
+
+        def __len__(self):
+            return self.num_samples
+
+    args = Namespace(batch_size=32)
+    args.classifier_opts = None
+    dataset = RandomDataset(100)  # 100 samples
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
+    args.dataloaders = {'train': dataloader, 'val': dataloader, 'test': dataloader}
+    args.probe_network = nn.Linear(1, 1)
+    split = 'train'
+
+    from uutils.torch_uu.metrics.diversity.task2vec_based_metrics.diversity_task2vec.diversity_for_few_shot_learning_benchmark import \
+        get_task_embeddings_from_few_shot_dataloader
+    embeddings: list[task2vec.Embedding] = get_task_embeddings_from_few_shot_dataloader(args,
+                                                                                        args.dataloaders,
+                                                                                        args.probe_network,
+                                                                                        num_tasks_to_consider=args.batch_size,
+                                                                                        split=split,
+                                                                                        classifier_opts=args.classifier_opts,
+                                                                                        )
+
+    # - compute distance matrix & task2vec based diversity, to demo` task2vec, this code computes pair-wise distance between task embeddings
+    distance_matrix: np.ndarray = task_similarity.pdist(embeddings, distance='cosine')
+    print(f'{distance_matrix=}')
+    from uutils.numpy_uu.common import get_diagonal
+    distances_as_flat_array, _, _ = get_diagonal(distance_matrix, check_if_symmetric=True)
+
+    # - compute div
+    from uutils.torch_uu.metrics.diversity.diversity import \
+        get_task2vec_diversity_coefficient_from_pair_wise_comparison_of_tasks, \
+        get_standardized_diversity_coffecient_from_pair_wise_comparison_of_tasks
+
+    div_tot = float(distances_as_flat_array.sum())
+    print(f'Diversity: {div_tot=}')
+    div, ci = get_task2vec_diversity_coefficient_from_pair_wise_comparison_of_tasks(distance_matrix)
+    print(f'Diversity: {(div, ci)=}')
+    standardized_div: float = get_standardized_diversity_coffecient_from_pair_wise_comparison_of_tasks(distance_matrix)
+    print(f'Standardised Diversity: {standardized_div=}')
+
+
 if __name__ == '__main__':
     import time
 
     start = time.time()
     # - run
     # compute_div_example1_test()
-    compute_size_aware_div_test_go()
+    # compute_size_aware_div_test_go()
+    test_tutorial()
     # - Done
     from uutils import report_times
 
