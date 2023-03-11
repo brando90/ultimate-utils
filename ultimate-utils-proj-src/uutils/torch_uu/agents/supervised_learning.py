usl vs maml trying to fix cuda memory issue, whatever I did here fixed it for 512, hopefully too for mds resnet50, likely removing the usl usl loss computation and making sure no pt tensors where around

ultimate-utils-proj-src/uutils/torch_uu/agents/supervised_learning.py
@@ -8,6 +8,7 @@ Note:
 """
 from argparse import Namespace
 
+import numpy as np
 import torch
 from torch import nn, Tensor
 
@@ -96,12 +97,12 @@ class ClassificationSLAgent(Agent):
 
         # -- convert losses & accs to list of floats
         if as_list_floats:
-            # todo: not sure if needed, but in the past keeping everything as tensors creates GPU memory issues since it never forget the computation graph
-            #   might be fine due to torch.no_grad() but not sure. Perhaps assert they are of type list[float]?
-            raise NotImplementedError
+            loss: list[float] = loss.detach().cpu().numpy().tolist()
+            acc: list[float] = acc.detach().cpu().numpy().tolist()
+
         if as_numpy_data:
-            loss = loss.detach().cpu().numpy()
-            acc = acc.detach().cpu().numpy()
+            loss: np.ndarray = loss.detach().cpu().numpy()
+            acc: np.ndarray = acc.detach().cpu().numpy()
 
         # - return loss to normal
         self.loss.reduction = original_reduction

ultimate-utils-proj-src/uutils/torch_uu/distributed.py
@@ -425,8 +425,10 @@ def move_model_to_ddp(rank: int, args: Namespace, model: nn.Module, force: bool 
     :return:
     """
     # - set device if not set, don't put in setup args because need to running this in it's own python process & rank if it's running parallel
+    if not hasattr(args, 'device'):
+        set_devices(args)  # this sets device if parallel or serial correctly using rank
     if args.device is None:
-        set_devices(args)  # this sets device if parallel or serial correctly
+        set_devices(args)  # this sets device if parallel or serial correctly using rank
     # - move model to device
     if is_running_parallel(rank) or force:
         # model.criterion = self.args.criterion.to(rank)  # I think its not needed since I already put it in the TP so when TP is moved to DDP the rank is moved automatically I hope

ultimate-utils-proj-src/uutils/torch_uu/meta_learners/pretrain_convergence.py
@@ -1,3 +1,5 @@
+from argparse import Namespace
+
 import torch
 import torch.nn as nn
 
@@ -30,10 +32,10 @@ from pdb import set_trace as st, set_trace
 class FitFinalLayer(nn.Module):
 
     def __init__(self,
-                 args,
-                 base_model,
-                 target_type='classification',
-                 classifier='LR',
+                 args: Namespace,
+                 base_model: nn.Module,
+                 target_type: str = 'classification',
+                 classifier: str = 'LR',
                  ):
         super().__init__()
         self.args = args