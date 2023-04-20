c1860190926c6d399da73873835cf835b4c5b413
Brando Miranda <brandojazz@gmail.com>
2023-04-20 16:14:18 -0700

    emergence plot 1

--- tutorials_for_myself/emergence_plots.py
+++ tutorials_for_myself/emergence_plots.py
@@ -1,52 +1,55 @@
 #%%
 """
-Plot acc = exp( -CE(N) ) where CE(N) = N^alpha / c^alpha.
-So y-xsis is acc = exp( -CE(N) ) [linear] and x-axis is N [log], in a log linear plot.
-Where Alpha = -1 < 0, N goes from 0 to 1_000, c = 2 > 0.
+In Python, plot
+    acc = exp( -CE(N) )^L
+where L (Length of Seq) goes from L=1 to 5 linearly in increments of 1 and,
+    CE(N) = N^alpha / c^alpha.
+So the y-axsis is acc = exp( -CE(N) ) [linear] and x-axis is log N (num. of parameters) [log]
+-- in a log linear plot, so use plt.xscale("log") and plt.yscale("linear").
+Where alpha = -1 (note alpha < 0),
+N goes from 1 to 1_000 (logarithmically) (note N > 0),
+c = 1 (note c > 0).
 Bound the y-axis between 0 and 1.
+Include in the legends the values of L.
+Use grids.
+Use a title.
+Use labels for the axes.
+Use a legend.
+Make sure to add comments explaining the code.
 Use python matplotlib.
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the range of N values, from 0 to 1000
-N = np.linspace(0, 1000, 1001)
+def CE(N, alpha, c):
+    return (N**alpha) / (c**alpha)
 
-# Define the alpha and c values
+def acc(CE, L):
+    return np.exp(-CE * L)
+
+# Parameters
 alpha = -1
-c = 2
+c = 1
+N = np.logspace(0, 3, 1000)  # N values from 1 to 1000 logarithmically
+L_values = range(1, 6)  # L values from 1 to 5
 
-# Calculate the CE(N) values using the formula N^alpha / c^alpha
-CE_N = (N ** alpha) / (c ** alpha)
-
-# Calculate the acc values using the formula exp(-CE(N))
-acc = np.exp(-CE_N)
-
-# Create a new figure
+# Create the plot
 plt.figure()
-
-# Plot the acc values against N values on a log-linear scale
-plt.plot(N, acc)
-
-# Set the x-axis scale to logarithmic
 plt.xscale("log")
-
-# Set the y-axis scale to linear
 plt.yscale("linear")
 
-# Set the y-axis limits between 0 and 1
-plt.ylim(0, 1)
+for L in L_values:
+    CE_values = CE(N, alpha, c)
+    acc_values = acc(CE_values, L)
+    plt.plot(N, acc_values, label=f"L={L}")
 
-# Add x-axis label
 plt.xlabel("N")
-
-# Add y-axis label
-plt.ylabel("acc = exp(-CE(N))")
-
-# Add a title to the plot
-plt.title("Log-linear plot of acc = exp(-CE(N))")
-
-# Display the plot
+plt.ylabel("acc")
+plt.title("Log-Linear Plot of acc vs N")
+plt.ylim(0, 1)
+plt.legend()
+plt.grid()
 plt.show()
+
 
