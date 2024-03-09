ac4d69541ad42dc7f6f4c00283f5e3bdc43381ef
Brando Miranda <brandojazz@gmail.com>
2024-03-09 11:38:50 -0800

    bit shift questions

---
+++ ultimate-utils-proj-src/uutils/coding_interviews/bit_manipulation/bit_playground.py
@@ -0,0 +1,24 @@
+def bit_shift():
+    # # -- Bit shift
+    # print(1 << 0)
+    # print(1 << 1)
+    # print(1 << 2)
+    print(-1 << 0)
+    print(-1 << 1)
+    print(-1 << 2)
+
+def bit_shift_and():
+    # -- And
+    # 100 & 100
+    print(4 & 4)
+    # 010 & 100
+    print(2 & 4)
+    # 100 & 101
+    print(4 & 5)
+    # 110 & 111
+    print(6 & 7)
+
+if __name__ == '__main__':
+    bit_shift()
+    # bit_shift_and()
+    print('Done!\a')
